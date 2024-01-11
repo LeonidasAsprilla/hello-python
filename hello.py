887da0cc7770fb7708c8726861d19eb845a293e8
LeonidasAsprilla <leonidasa@uninorte.edu.co>
2024-01-11 16:21:30 -0500

    Tipos de datos en Python

--- hello.py
+++ hello.py
@@ -1,9 +1,27 @@
 # Esto es un comentario:
 # Hola mundo!
 print("Hola Mundo!")
+print('Hola python!')
 
 """
 Este es un
 comentario 
 en varias lineas
 """
+'''
+Esto tambien
+es un comentario
+en varias lineas
+'''
+
+# Consultar el tipo de dato
+# type -> tipo o clase de dato
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Leonidas Asprilla'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Leonidas'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
