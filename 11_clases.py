e6b4f12f066ba6b2a1239372ede871bca5198601
LeonidasAsprilla <leonidasa@uninorte.edu.co>
2024-01-14 19:43:16 -0500

    Clases en Python

---
+++ 11_clases.py
@@ -0,0 +1,133 @@
+# Clases
+# Creando una clase
+class Person:
+  pass
+print(Person) # <class '__main__.Person'>
+
+# Creando un objeto
+class Person:
+      def __init__ (self, name):
+        # self allows to attach parameter to the class
+          self.name =name
+
+p = Person('Leonidas')
+print(p.name)
+print(p)
+
+# Añadiendo mas parametros a la clase persona
+class Person:
+      def __init__(self, firstname, lastname, age, country, city):
+          self.firstname = firstname
+          self.lastname = lastname
+          self.age = age
+          self.country = country
+          self.city = city
+
+
+p = Person('Leonidas', 'Asprilla', 42, 'Colombia', 'Ciénaga')
+print(p.firstname)
+print(p.lastname)
+print(p.age)
+print(p.country)
+print(p.city)
+
+# Definiendo metodos de clase, que los objetos pueden usar
+class Person:
+      def __init__(self, firstname, lastname, age, country, city):
+          self.firstname = firstname
+          self.lastname = lastname
+          self.age = age
+          self.country = country
+          self.city = city
+      def person_info(self):
+        return f'{self.firstname} {self.lastname} is {self.age} years old. He lives in {self.city}, {self.country}'
+
+p = Person('Leonidas', 'Asprilla', 42, 'Colombia', 'Ciénaga')
+print(p.person_info())
+
+# Objetos con valores por defecto 
+class Person:
+      def __init__(self, firstname='Asabeneh', lastname='Yetayeh', age=250, country='Finland', city='Helsinki'):
+          self.firstname = firstname
+          self.lastname = lastname
+          self.age = age
+          self.country = country
+          self.city = city
+
+      def person_info(self):
+        return f'{self.firstname} {self.lastname} is {self.age} years old. He lives in {self.city}, {self.country}.'
+
+p1 = Person()
+print(p1.person_info())
+p2 = Person('Leonidas', 'Asprilla', 42, 'Colombia', 'Ciénaga')
+print(p2.person_info())
+
+# Metodo para modificar los valores por defecto de la clase
+class Person:
+      def __init__(self, firstname='Asabeneh', lastname='Yetayeh', age=250, country='Finland', city='Helsinki'):
+          self.firstname = firstname
+          self.lastname = lastname
+          self.age = age
+          self.country = country
+          self.city = city
+          self.skills = []
+
+      def person_info(self):
+        return f'{self.firstname} {self.lastname} is {self.age} years old. He lives in {self.city}, {self.country}.'
+      def add_skill(self, skill):
+          self.skills.append(skill)
+
+p1 = Person()
+print(p1.person_info())
+p1.add_skill('HTML')
+p1.add_skill('CSS')
+p1.add_skill('JavaScript')
+p2 = Person('Leonidas', 'Asprilla', 42, 'Colombia', 'Ciénaga')
+print(p2.person_info())
+print(p1.skills)
+print(p2.skills)
+
+""" Herencia
+Using inheritance we can reuse parent class code. Inheritance allows us to define a class that inherits all the methods and properties from parent class. The parent class or super or base class is the class which gives all the methods and properties. Child class is the class that inherits from another or parent class. Let us create a student class by inheriting from person class. """
+
+class Student(Person):
+    pass
+
+s1 = Student('Eyob', 'Yetayeh', 30, 'Finland', 'Helsinki')
+s2 = Student('Lidiya', 'Teklemariam', 28, 'Finland', 'Espoo')
+
+print(s1.person_info())
+s1.add_skill('JavaScript')
+s1.add_skill('React')
+s1.add_skill('Python')
+print(s1.skills)
+
+print(s2.person_info())
+s2.add_skill('Organizing')
+s2.add_skill('Marketing')
+s2.add_skill('Digital Marketing')
+print(s2.skills)
+
+# Sobreescribiendo metodos de la clase padre
+class Student(Person):
+    def __init__ (self, firstname='Asabeneh', lastname='Yetayeh',age=250, country='Finland', city='Helsinki', gender='male'):
+        self.gender = gender
+        super().__init__(firstname, lastname,age, country, city)
+    def person_info(self):
+        gender = 'He' if self.gender =='male' else 'She'
+        return f'{self.firstname} {self.lastname} is {self.age} years old. {gender} lives in {self.city}, {self.country}.'
+
+s1 = Student('Eyob', 'Yetayeh', 30, 'Finland', 'Helsinki','male')
+s2 = Student('Lidiya', 'Teklemariam', 28, 'Finland', 'Espoo', 'female')
+print(s1.person_info())
+s1.add_skill('JavaScript')
+s1.add_skill('React')
+s1.add_skill('Python')
+print(s1.skills)
+
+print(s2.person_info())
+s2.add_skill('Organizing')
+s2.add_skill('Marketing')
+s2.add_skill('Digital Marketing')
+print(s2.skills)
+
